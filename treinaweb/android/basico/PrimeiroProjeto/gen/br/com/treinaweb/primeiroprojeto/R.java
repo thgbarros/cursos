cbeae58ca037b36a589e911b7ce3ea833bcb12c7
Thiago barros <thg.sbarros@gmail.com>
2015-04-10 17:46:03 -0300

    62% - ícones nativo

--- treinaweb/android/basico/PrimeiroProjeto/gen/br/com/treinaweb/primeiroprojeto/R.java
+++ treinaweb/android/basico/PrimeiroProjeto/gen/br/com/treinaweb/primeiroprojeto/R.java
@@ -9,51 +9,92 @@ package br.com.treinaweb.primeiroprojeto;
 
 public final class R {
     public static final class array {
-        public static final int cursos=0x7f050000;
+        public static final int cursos=0x7f060000;
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int amarelo=0x7f040008;
+        public static final int azul=0x7f040000;
+        public static final int branco=0x7f040001;
+        public static final int cinza=0x7f040002;
+        public static final int laranja=0x7f040004;
+        public static final int preto=0x7f040003;
+        public static final int rosa=0x7f040006;
+        public static final int verde=0x7f040007;
+        public static final int vermelho=0x7f040005;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_archive=0x7f020001;
+        public static final int ic_menu_attachment=0x7f020002;
+        public static final int ic_menu_delete=0x7f020003;
+        public static final int ic_menu_edit=0x7f020004;
+        public static final int ic_menu_home=0x7f020005;
+        public static final int ic_menu_more=0x7f020006;
+        public static final int ic_menu_save=0x7f020007;
+        public static final int ic_menu_search=0x7f020008;
     }
     public static final class id {
-        public static final int buttonOk=0x7f07000c;
-        public static final int buttonProximo=0x7f070002;
-        public static final int buttonVoltar=0x7f070008;
-        public static final int editText1=0x7f07000a;
-        public static final int extTextNome=0x7f070001;
-        public static final int listViewCursos=0x7f07000e;
-        public static final int tableRow1=0x7f070009;
-        public static final int tableRow2=0x7f07000b;
-        public static final int textView1=0x7f070004;
-        public static final int textView2=0x7f070005;
-        public static final int textView3=0x7f070006;
-        public static final int textViewAluno=0x7f070007;
-        public static final int textViewMensagem=0x7f070000;
-        public static final int textViewResposta=0x7f070003;
-        public static final int textViewRotulo=0x7f07000d;
+        public static final int buttonOk=0x7f09000c;
+        public static final int buttonProximo=0x7f090002;
+        public static final int buttonVoltar=0x7f090008;
+        public static final int editText1=0x7f09000a;
+        public static final int extTextNome=0x7f090001;
+        public static final int listViewCursos=0x7f09000e;
+        public static final int menu_archive=0x7f090012;
+        public static final int menu_delete=0x7f090016;
+        public static final int menu_edit=0x7f090013;
+        public static final int menu_home=0x7f090015;
+        public static final int menu_more=0x7f090017;
+        public static final int menu_save=0x7f090014;
+        public static final int spinnerCursos=0x7f09000f;
+        public static final int tableRow1=0x7f090009;
+        public static final int tableRow2=0x7f09000b;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090005;
+        public static final int textView3=0x7f090006;
+        public static final int textViewAluno=0x7f090007;
+        public static final int textViewMensagem=0x7f090000;
+        public static final int textViewResosta=0x7f090010;
+        public static final int textViewResposta=0x7f090003;
+        public static final int textViewRotulo=0x7f09000d;
+        public static final int textViewTitulo=0x7f090011;
     }
     public static final class layout {
         public static final int activity_tela1=0x7f030000;
         public static final int activity_tela2=0x7f030001;
         public static final int activity_tela3=0x7f030002;
         public static final int activity_tela6=0x7f030003;
+        public static final int activity_tela7=0x7f030004;
+        public static final int activity_tela8=0x7f030005;
+    }
+    public static final class menu {
+        public static final int activity_tela1=0x7f080000;
     }
     public static final class string {
-        public static final int aluno=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int botao_ok=0x7f040007;
-        public static final int botao_proximo=0x7f040006;
-        public static final int botao_voltar=0x7f04000b;
-        public static final int curso=0x7f040003;
-        public static final int ensino=0x7f040009;
-        public static final int msg=0x7f040004;
-        public static final int nome=0x7f04000c;
-        public static final int ola_mundo=0x7f040001;
-        public static final int resposta=0x7f040008;
-        public static final int rotulo=0x7f04000d;
-        public static final int text_nome=0x7f040005;
-        public static final int titulo=0x7f04000a;
+        public static final int aluno=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int botao_ok=0x7f050007;
+        public static final int botao_proximo=0x7f050006;
+        public static final int botao_voltar=0x7f05000b;
+        public static final int curso=0x7f050003;
+        public static final int ensino=0x7f050009;
+        public static final int menu_archive=0x7f05000e;
+        public static final int menu_attachment=0x7f05000f;
+        public static final int menu_delete=0x7f050010;
+        public static final int menu_edit=0x7f050011;
+        public static final int menu_home=0x7f050012;
+        public static final int menu_more=0x7f050013;
+        public static final int menu_save=0x7f050014;
+        public static final int menu_search=0x7f050015;
+        public static final int msg=0x7f050004;
+        public static final int nome=0x7f05000c;
+        public static final int ola_mundo=0x7f050001;
+        public static final int resposta=0x7f050008;
+        public static final int rotulo=0x7f05000d;
+        public static final int text_nome=0x7f050005;
+        public static final int titulo=0x7f05000a;
     }
     public static final class style {
         /** 
@@ -77,10 +118,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
