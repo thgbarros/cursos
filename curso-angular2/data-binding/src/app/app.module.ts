b08ab7b0ee5bcc5c39e94ac43dae3d4b23437d59
Thiago Barros <thiago@tecnomotor.com.br>
2017-10-30 20:02:55 -0200

    aula de two-way databinding

---
+++ curso-angular2/data-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
